3e26f5ebd1a1535527ae2a7bccb9a7cd900eb120
Leo <leo_nd@hotmail.fr>
2015-10-12 19:02:28 +0200

    modification inclinaison motion sans répétition

--- Session5BleProject/App/Motion/motion.c
+++ Session5BleProject/App/Motion/motion.c
@@ -10,6 +10,8 @@
 #include "stm32f4xx_nucleo.h"
 #include "mems.h"
 
+static int accAv = -1;
+
 void Motion_Init(void) {
 	Mems_StartReadSensors(10, ACCELEROMETER_SENSOR);
 }
@@ -19,22 +21,46 @@ void Motion_Process(void) {
 }
 
 void Mems_Acc_CB(int32_t acc_x, int32_t acc_y, int32_t acc_z) {
+	/*Variables*/
+	int acc;
 	int angle30 = 1000/3;
 	int angle60 = 1000/3*2;
 	int dec = 100;
+	/*Immobilité par rapport au sol*/
+	if(fabs(acc_x)<dec && fabs(acc_y)<dec) acc=0;
 	/*Inclinaison gauche/droite*/
-	if(fabs(acc_x)<dec) Motion_InclinationLeftRightAxisNone_CB();
-	if (fabs(acc_x-angle30)<dec) Motion_InclinationLeftRightAxisLeftMedium_CB();
-	if (fabs(acc_x-angle60)<dec) Motion_InclinationLeftRightAxisLeftHigh_CB();
-	if (fabs(acc_x+angle30)<dec) Motion_InclinationLeftRightAxisRightMedium_CB();
-	if (fabs(acc_x+angle60)<dec) Motion_InclinationLeftRightAxisRightHigh_CB();
+	else if (fabs(acc_x-angle30)<dec) acc=1;
+	else if (fabs(acc_x-angle60)<dec) acc=2;
+	else if (fabs(acc_x+angle30)<dec) acc=3;
+	else if (fabs(acc_x+angle60)<dec) acc=4;
 	/*Inclinaison avant/arrière*/
-	if (fabs(acc_y)<dec) Motion_InclinationBackFrontAxisNone_CB();
-	if (fabs(acc_y-angle30)<dec) Motion_InclinationBackFrontAxisBackMedium_CB();
-	if (fabs(acc_y-angle60)<dec) Motion_InclinationBackFrontAxisBackHigh_CB();
-	if (fabs(acc_y+angle30)<dec) Motion_InclinationBackFrontAxisFrontMedium_CB();
-	if (fabs(acc_y+angle60)<dec) Motion_InclinationBackFrontAxisFrontHigh_CB();
+	else if (fabs(acc_y-angle30)<dec) acc=6;
+	else if (fabs(acc_y-angle60)<dec) acc=7;
+	else if (fabs(acc_y+angle30)<dec) acc=8;
+	else if (fabs(acc_y+angle60)<dec) acc=9;
+	else acc = accAv;
+
+	/*Si le device change d'inclinaison*/
+	if(acc!=accAv){
+		accAv = acc;
+		switch(acc){
+		case 0 : Motion_InclinationLeftRightAxisNone_CB();
+		Motion_InclinationBackFrontAxisNone_CB(); break;
+		case 1 : Motion_InclinationLeftRightAxisLeftMedium_CB(); break;
+		case 2 : Motion_InclinationLeftRightAxisLeftHigh_CB(); break;
+		case 3 : Motion_InclinationLeftRightAxisRightMedium_CB(); break;
+		case 4 : Motion_InclinationLeftRightAxisRightHigh_CB();break;
+		case 6 : Motion_InclinationBackFrontAxisBackMedium_CB(); break;
+		case 7 : Motion_InclinationBackFrontAxisBackHigh_CB(); break;
+		case 8 : Motion_InclinationBackFrontAxisFrontMedium_CB(); break;
+		case 9 : Motion_InclinationBackFrontAxisFrontHigh_CB(); break;
+		}
+
+	}
+
 }
+
+
 
 __weak void Motion_InclinationLeftRightAxisNone_CB(void) {
 	DEBUG_LINE("Inclination axis left right : none");
