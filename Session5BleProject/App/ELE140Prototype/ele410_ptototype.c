440c8b9489353fe31601106179c2022ee7313fe3
Leo <leo_nd@hotmail.fr>
2015-10-17 13:49:11 +0200

    debut undirected connectable mode

--- Session5BleProject/App/ELE140Prototype/ele410_ptototype.c
+++ Session5BleProject/App/ELE140Prototype/ele410_ptototype.c
@@ -19,5 +19,15 @@ void Button_Button_Long_Pressed_CB(void)
 {
 
 	BLE_Common_Set_Discoverable();
+	gp_timer_t timer;
+		//Timer_Set(&timer,interval);
+		tClockTime interval = 10000;
+		int delay = (int) (Clock_Time()-time);
+	DEBUG_LINE("reste : %d",(int)delay);
+	//if(delay>interval)
+		DEBUG_LINE("reste : %d",(int)delay);
+		//aci_gap_set_undirected_connectable(PUBLIC_ADDR,ADV_IND);
+		//aci_gap_set_undirected_connectable(NO_WHITE_LIST_USE ,PUBLIC_ADDR);
+		aci_gap_set_undirected_connectable(0x03,0x00);
 
 }
